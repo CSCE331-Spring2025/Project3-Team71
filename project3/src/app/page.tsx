49d1ae268c900e9ccde1006ab3ea454873874df8
Tanmay Chhimwal <chhimwalt@gmail.com>
2025-03-19 11:58:46 -0500

    linking example

--- project3/src/app/page.tsx
+++ project3/src/app/page.tsx
@@ -1,3 +1,21 @@
+import Link from 'next/link'
+
+
 export default function Page() {
-  return <h1>This is home page?</h1>
+  return (
+    <div>
+    <h1>This is home page</h1>
+        <p>Welcome to Tea71</p>
+        <p>Choose your role to continue:</p>
+        <br />
+        <Link href={`/customer`}>Customer</Link>
+        <br />
+        <br />
+        <Link href={`/cashier`}>Cashier</Link>
+        <br />
+        <br />
+        <Link href={`/manager`}>Manager</Link>
+    </div>
+  )
 }
+
