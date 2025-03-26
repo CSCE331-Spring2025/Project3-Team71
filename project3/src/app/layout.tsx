28b7c7a1980293417330fbcf96fc15d034aa45b8
Tanmay Chhimwal <chhimwalt@gmail.com>
2025-03-26 09:18:09 -0500

    fixed nav bar nesting issue

--- project3/src/app/layout.tsx
+++ project3/src/app/layout.tsx
@@ -25,8 +25,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <NavBar />
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
