fd6daf5ca2b2395b954e2dea02c7b552a991ded5
Anthony Guzman <123406289+GuzmanAnthony@users.noreply.github.com>
2025-03-27 09:54:08 -0500

    Update postcss.config.mjs

--- project3/postcss.config.mjs
+++ project3/postcss.config.mjs
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
+  plugins: [tailwindcss, autoprefixer],
 }
