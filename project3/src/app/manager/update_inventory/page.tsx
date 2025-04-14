f6e2d3fdebf6ab5882c1df8928ad51ced6fa400f
Tanmay Chhimwal <chhimwalt@gmail.com>
2025-04-14 12:49:13 -0500

    fix background color on update inventory, add recharts

--- project3/src/app/manager/update_inventory/page.tsx
+++ project3/src/app/manager/update_inventory/page.tsx
@@ -118,7 +118,7 @@ export default function UpdateInventoryPage() {
   }
 
   return (
-    <div className="p-6 min-h-screen bg-gray-50">
+    <div className="p-6 min-h-screen bg-background">
       <h1 className="text-3xl font-bold text-center mb-6">Update Inventory</h1>
 
       {/* Toggle Threshold Settings */}
